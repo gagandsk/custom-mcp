d031f9bcb7e5666f2782e3358a55b54fa34aa1ee
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-05-21 17:59:15 +0200

    Add weather API integration

--- main.ts
+++ main.ts
@@ -16,14 +16,33 @@ server.tool(
     city: z.string().describe("City name"),
   },
   async ({ city }) => {
+    const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`)
+    const data = await response.json()
+    
+    if (data.length === 0) {
+        return {
+            content: [
+                {
+                    type: "text",
+                    text: `No se encontró la información para la ciudad ${city}`,
+                },
+            ],
+        };
+    }
+
+    const { latitude, longitude } = data.results[0]
+    const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current=temperature_2m,precipitation,is_day,rain&forecast_days=1`)
+    const weatherData = await weatherResponse.json()
+    
     return {
-      content: [
-        {
-          type: "text",
-          text: `El clima de ${city} es soleado`,
-        },
-      ],
+        content: [
+            {
+            type: "text",
+            text: JSON.stringify(weatherData, null, 2),
+            },
+        ],
     };
+    
   }
 );
 
